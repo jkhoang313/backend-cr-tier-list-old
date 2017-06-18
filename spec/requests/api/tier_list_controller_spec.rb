bbdbfbfdeac85a098b05ac4c360c79f793ae2592
jkhoang313 <jkhoang313@gmail.com>
2017-06-18 12:56:14 -0400

    Added rspec

---
+++ spec/requests/api/tier_list_controller_spec.rb
@@ -0,0 +1,12 @@
+# require 'rails_helper'
+#
+# RSpec.describe "Tier List API", :type => :request do
+#   describe 'GET /api/tier_lists' do
+#     it 'should return all tier lists' do
+#       get("/api/tier_lists")
+#
+#       body = JSON.parse(response.body)
+#
+#     end
+#   end
+# end
