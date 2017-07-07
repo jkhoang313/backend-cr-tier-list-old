dbd05f9a4805014dc955d3469d49790fb3760daa
jkhoang313 <jkhoang313@gmail.com>
2017-07-06 22:53:29 -0400

    Serializes date in strtime

--- app/serializers/tier_list_serializer.rb
+++ app/serializers/tier_list_serializer.rb
@@ -5,4 +5,12 @@ class TierListSerializer < ActiveModel::Serializer
   def tiers
     object.tier_positions
   end
+
+  def created_at
+    object.created_at.strftime("%-m-%-d-%Y")
+  end
+
+  def updated_at
+    object.updated_at.strftime("%-m-%-d-%Y")
+  end
 end
